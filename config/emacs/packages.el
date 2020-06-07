ba8c1c8f9f293577f60644c2c40f0fa5583696eb
Martin Baillie <martin@baillie.email>
2020-06-07 21:32:54 +1000

    Finish up Emacs motion trainer code
    Signed-off-by: Martin Baillie <martin@baillie.email>

--- config/emacs/packages.el
+++ config/emacs/packages.el
@@ -3,28 +3,30 @@
 
 (package! dap-mode)
 (package! nyan-mode)
-;; (package! emojify)
-
 (package! org-fancy-priorities)
 (package! org-trello)
-;; (package! fence-edit)
 (package! edit-indirect)
-
 (package! pinentry)
 (package! deadgrep)
 (package! ripgrep)
-
 (package! move-text)
 (package! buffer-move)
 (package! buffer-expose)
 (package! exec-path-from-shell)
 (package! flycheck-golangci-lint)
 
-(unpin! vterm)
+(package! evil-motion-trainer :pin nil :recipe
+  (:host github :repo "martinbaillie/evil-motion-trainer"))
+  ;; :recipe (:local-repo "/home/martin/Code/personal/evil-motion-trainer"
+           ;; :files ("*.el")))
 
-(unpin! evil-easymotion)
+;; Use my fork of evil-easymotion.
 (package! evil-easymotion :pin nil :recipe
   (:host github :repo "martinbaillie/evil-easymotion"))
 
+;; Use my fork of kubectx-mode.
 (package! kubectx-mode :pin nil :recipe
   (:host github :repo "martinbaillie/emacs-kubectx-mode"))
+
+;; Use bleeding edge vterm
+(unpin! vterm)
