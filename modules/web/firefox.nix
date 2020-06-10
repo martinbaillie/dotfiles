1e3e345dab4d0acadc8eb0cdad06805e2b0829f2
Martin Baillie <martin@baillie.email>
2020-06-10 20:03:58 +1000

    Add browser mime type
    Signed-off-by: Martin Baillie <martin@baillie.email>

--- modules/web/firefox.nix
+++ modules/web/firefox.nix
@@ -9,6 +9,19 @@ in mkMerge [
   (mkIf isLinux {
     my = {
       packages = [ pkgs.firefox-wayland ];
+      home.xdg.mimeApps.defaultApplications = {
+        "application/x-extension-htm" = [ "firefox.desktop" ];
+        "application/x-extension-html" = [ "firefox.desktop" ];
+        "application/x-extension-shtml" = [ "firefox.desktop" ];
+        "application/x-extension-xht" = [ "firefox.desktop" ];
+        "application/x-extension-xhtml" = [ "firefox.desktop" ];
+        "application/xhtml+xml" = [ "firefox.desktop" ];
+        "text/html" = [ "firefox.desktop" ];
+        "x-scheme-handler/chrome" = [ "firefox.desktop" ];
+        "x-scheme-handler/ftp" = [ "firefox.desktop" ];
+        "x-scheme-handler/http" = [ "firefox.desktop" ];
+        "x-scheme-handler/https" = [ "firefox.desktop" ];
+      };
       env.XDG_DESKTOP_DIR = "$HOME"; # prevent creation of ~/Desktop
     };
   })
