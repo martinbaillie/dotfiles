41edb244ac198dc4359160426d487e66b5108f2b
Martin Baillie <martin@baillie.email>
2020-06-19 20:52:58 +1000

    Move off Kube helm entirely
    Signed-off-by: Martin Baillie <martin@baillie.email>

--- modules/ops/kubernetes.nix
+++ modules/ops/kubernetes.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }: {
   my = {
-    packages = with pkgs; [ kubectl kubectx kubernetes-helm kustomize ];
+    packages = with pkgs; [
+      kubectl
+      kubectx
+      # kubernetes-helm
+      kustomize
+    ];
     home.xdg.configFile."zsh/rc.d/rc.kubernetes.zsh".source =
       <config/kubernetes/rc.zsh>;
   };
