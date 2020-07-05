5bf60d161b62fde925616b48cffe8aae6c0876c7
Martin Baillie <martin@baillie.email>
2020-07-05 18:03:19 +1000

    Remove Jupyter for now
    Signed-off-by: Martin Baillie <martin@baillie.email>

--- modules/dev/python.nix
+++ modules/dev/python.nix
@@ -5,26 +5,26 @@ let
 in {
   my = {
     packages = with pkgs; [
-      (jupyter.override {
-        definitions = {
-          python3 = let
-            env = (pkgs.python37.withPackages
-              (ps: with ps; [ ipykernel ipython matplotlib pandas numpy ]));
-          in {
-            displayName = "Python 3";
-            argv = [
-              "${env.interpreter}"
-              "-m"
-              "ipykernel_launcher"
-              "-f"
-              "{connection_file}"
-            ];
-            language = "python";
-            logo32 = "${env.sitePackages}/ipykernel/resources/logo-32x32.png";
-            logo64 = "${env.sitePackages}/ipykernel/resources/logo-64x64.png";
-          };
-        };
-      })
+      # (jupyter.override {
+      #   definitions = {
+      #     python3 = let
+      #       env = (pkgs.python37.withPackages
+      #         (ps: with ps; [ ipykernel ipython matplotlib pandas numpy ]));
+      #     in {
+      #       displayName = "Python 3";
+      #       argv = [
+      #         "${env.interpreter}"
+      #         "-m"
+      #         "ipykernel_launcher"
+      #         "-f"
+      #         "{connection_file}"
+      #       ];
+      #       language = "python";
+      #       logo32 = "${env.sitePackages}/ipykernel/resources/logo-32x32.png";
+      #       logo64 = "${env.sitePackages}/ipykernel/resources/logo-64x64.png";
+      #     };
+      #   };
+      # })
       (python3.withPackages (ps: with ps; [ black pip pylint setuptools ]))
       (mkIf isLinux (unstable.python-language-server))
       # REVIEW: Language server is currently bust on Darwin.
