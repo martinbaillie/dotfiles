0936a238284e8dcdb35898d0540491dbae45c378
Martin Baillie <martin@baillie.email>
2020-07-09 21:09:25 +1000

    Extract theme colours into files
    Signed-off-by: Martin Baillie <martin@baillie.email>

--- modules/themes/dark/default.nix
+++ modules/themes/dark/default.nix
@@ -10,35 +10,7 @@ in {
   theme = mkMerge [
     {
       wallpaper = ./dj_nobu_dark.jpg;
-
-      # Doom One Dark.
-      colours = {
-        bg = "#282c34";
-        bgalt = "#21242b";
-        base0 = "#1B2229";
-        base1 = "#1c1f24";
-        base2 = "#202328";
-        base3 = "#23272e";
-        base4 = "#3f444a";
-        base5 = "#5B6268";
-        base6 = "#73797e";
-        base7 = "#9ca0a4";
-        base8 = "#DFDFDF";
-        fg = "#bbc2cf";
-        fgalt = "#5B6268";
-        grey = "#3f444a";
-        red = "#ff6c6b";
-        orange = "#da8548";
-        green = "#98be65";
-        teal = "#4db5bd";
-        yellow = "#ECBE7B";
-        blue = "#51afef";
-        darkblue = "#2257A0";
-        magenta = "#c678dd";
-        violet = "#a9a1e1";
-        cyan = "#46D9FF";
-        darkcyan = "#5699AF";
-      };
+      colours = import ./doom_dracula.nix;
     }
 
     (mkIf isLinux {
---
+++ modules/themes/dark/doom_dracula.nix
@@ -0,0 +1,28 @@
+# Doom Dracula
+{
+  bg = "#282a36";
+  bgalt = "#1E2029";
+  base0 = "#1E2029";
+  base1 = "#282a36";
+  base2 = "#373844";
+  base3 = "#44475a";
+  base4 = "#565761";
+  base5 = "#6272a4";
+  base6 = "#b6b6b2";
+  base7 = "#ccccc7";
+  base8 = "#f8f8f2";
+  fg = "#f8f8f2";
+  fgalt = "#e2e2dc";
+  grey = "#565761";
+  red = "#ff5555";
+  orange = "#ffb86c";
+  green = "#50fa7b";
+  teal = "#0189cc";
+  yellow = "#f1fa8c";
+  blue = "#61bfff";
+  darkblue = "#0189cc";
+  magenta = "#ff79c6";
+  violet = "#bd93f9";
+  cyan = "#8be9fd";
+  darkcyan = "#8be9fd";
+}
