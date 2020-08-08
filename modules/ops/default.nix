6ba3aa934babcf228e9b3acf035a4ebbca28d207
Martin Baillie <martin@baillie.email>
2020-08-08 16:52:39 +1000

    Switch back to `awscli` v1
    Signed-off-by: Martin Baillie <martin@baillie.email>

--- modules/ops/default.nix
+++ modules/ops/default.nix
@@ -1,17 +1,14 @@
-{ lib, pkgs, ... }:
-let
-  inherit (lib.systems.elaborate { system = builtins.currentSystem; }) isLinux;
-in {
+{ lib, pkgs, ... }: {
   my = {
     packages = with pkgs; [
+      awscli
+      dhall
+      dhall-json
+      haskellPackages.dhall-lsp-server
       terraform
       terraform-lsp
       vault
       yaml-language-server
-      dhall
-      dhall-json
-      haskellPackages.dhall-lsp-server
-      (if isLinux then awscli else awscli2)
     ];
     home.xdg.configFile."zsh/rc.d/rc.terraform.zsh".source =
       <config/terraform/rc.zsh>;
